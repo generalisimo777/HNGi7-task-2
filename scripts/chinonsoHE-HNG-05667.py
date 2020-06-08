9e53b0cfdab907257d0687eecf1e647cbde90a02
Chinonso Egejuru <echenri@gmail.com>
2020-06-08 13:05:13 +0100

    Remove fullstop and other errors in output msg

--- scripts/chinonsoHE-HNG-05667.py
+++ scripts/chinonsoHE-HNG-05667.py
@@ -1,7 +1,7 @@
 # Stage 2 Entry Task 
 
 def runTaskOne():
-    return print("Hello World, this is {} with HNGi7 ID {} and email {} using {} for stage 2 task.".format("Chinonso Egejuru", "HNG-05667", "echenri@gmail.com", "Python 3"))
+    return print("Hello World, this is {} with HNGi7 ID {} and email {} using {} for stage 2 task".format("Chinonso Egejuru", "HNG-05667", "echenri@gmail.com", "Python"))
     
 
 if __name__ == "__main__":
