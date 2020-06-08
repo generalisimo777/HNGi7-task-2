bd0725d70e713f2ea0354ad3b045e44addf130b6
Tomiwa97 <ajayitomiwa71@gmail.com>
2020-06-08 08:55:23 +0100

    File Updates

--- scripts/ajayiakeem-HNG-06793.py
+++ scripts/ajayiakeem-HNG-06793.py
@@ -1,1 +1,1 @@
-print("Hello world, this is Ajayi Akeem with HNGi7 ID HNG-06793 email ajayiakeem34@gmail.com using python python for stage 2 task")
+print("Hello World, this is Ajayi Akeem with HNGi7 ID HNG-06793 email ajayiakeem34@gmail.com using python for stage 2 task")
